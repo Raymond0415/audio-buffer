391cbc4325093caa94b8cdf43bbdee9e67ea9b38
Raymond-intramotev <ruilin.zhang@intramotev.com>
2025-04-19 14:40:41 -0500

    Ensure consistent config for output/input.

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,14 @@ fn main() -> Result<(), anyhow::Error> {
     let input_device = host.default_input_device().expect("no input device");
     let output_device = host.default_output_device().expect("no output device");
 
-    let input_cfg = input_device
-    .default_input_config()
-    .unwrap_or_else(|e| {
-        eprintln!("Input config error: {:?}", e);
-        std::process::exit(1);
-    });
-    let output_cfg = output_device.default_output_config().expect("no input config");
-
-    println!("Default input config: {:?}", input_cfg);
-    println!("Default output config: {:?}", output_cfg);
-
+    let input_cfg = input_device.default_input_config()?;
     let sample_rate = input_cfg.sample_rate();
-    let sample_format = input_cfg.sample_format();
     let channels = input_cfg.channels();
-
-    let output_config = cpal::StreamConfig {
+    let output_cfg = cpal::StreamConfig {
         channels,
         sample_rate,
         buffer_size: cpal::BufferSize::Default,
     };
-
-    println!("sample rate: {}, format: {:?}, channels: {}", sample_rate.0, sample_format, channels);
 
     let buffer = HeapRb::<f32>::new(sample_rate.0 as usize / 20);
     let (mut producer, mut consumer) = buffer.split();
@@ -46,7 +32,7 @@ fn main() -> Result<(), anyhow::Error> {
     )?;
 
     let output_stream = output_device.build_output_stream(
-        &output_config,
+        &output_cfg,
         move |data: &mut [f32], _| {
             for sample in data.iter_mut() {
                 *sample = consumer.try_pop().unwrap_or(0.0);
